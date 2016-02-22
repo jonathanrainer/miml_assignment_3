ca21d01449692906c969ade96920a64ca3fc3c47
Jonathan Rainer <jonrainer@googlemail.com>
2016-02-22 22:28:14 +0000

    "Made small changes to how the holling function is calculated."

--- matlab_code/q1/tbderivs.m
+++ matlab_code/q1/tbderivs.m
@@ -1,11 +1,10 @@
 function outs = tbderivs(t, pops, r, K, R_m, alpha, gamma, mu)
 %TBDERIVS Evaluate derivative in Truscott and Brindley, Eqns (2)+(3)
 
-    holling_function = (pops(1)^2/(alpha^2 + pops(1)^2));
+    holling_function = R_m * pops(2) * ((pops(1)^2)/(alpha^2 + pops(1)^2));
 
-    dPdt = (r*pops(1) * (1 - pops(1)/K)) - ... 
-        (R_m * pops(2) * holling_function);
-    dZdt = gamma * R_m * pops(2) * holling_function - mu*pops(2);
+    dPdt = (r*pops(1) * (1 - (pops(1)/K))) - holling_function;
+    dZdt = gamma * holling_function - mu*pops(2);
 
     outs = [dPdt ; dZdt];
 end
