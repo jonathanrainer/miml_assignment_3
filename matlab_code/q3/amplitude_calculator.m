86d16361f3d31e96c955ba331903945b76ec2c18
Jonathan Rainer <jonrainer@googlemail.com>
2016-02-22 22:29:45 +0000

    "Added in first draft of answers to Q3."

---
+++ matlab_code/q3/amplitude_calculator.m
@@ -0,0 +1,24 @@
+function A = amplitude_calculator(time_span, ...
+    transience_period, accuracy, a_vals)
+
+    K = 108;
+    alpha = 5.7;
+    gamma = 0.05;
+    mu = 0.012;
+    R_m = 0.7;
+
+    yStart = rand(1, 2);
+
+    while (a_vals(2) - a_vals(1) > accuracy)
+      A = mean(a_vals);
+      [~, out_z] = ode45(@(t,y) tbderivs_forced(t,y, A, K, R_m, alpha, ...
+          gamma, mu), time_span, yStart);
+
+      if (max(out_z(transience_period:end, 1)) > 50)
+        a_vals(2) = A;
+      else
+        a_vals(1) = A;
+      end
+    end
+end
+
---
+++ matlab_code/q3/tbderivs_forced.m
@@ -0,0 +1,13 @@
+function outs = tbderivs_forced(t, pops, A, K, R_m, alpha, gamma, mu)
+%TBDERIVS Evaluate derivative in Truscott and Brindley, Eqns (2)+(3)
+
+    holling_function = R_m * pops(2) * ((pops(1)^2)/(alpha^2 + pops(1)^2));
+
+    r = r_func(t, A, 0.3);
+    
+    dPdt = (r*pops(1) * (1 - (pops(1)/K))) - holling_function;
+    dZdt = (gamma * holling_function) - mu*pops(2);
+
+    outs = [dPdt ; dZdt];
+end
+
